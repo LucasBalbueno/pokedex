2ce8f6bb3e594d3d15c00b83a900f8a22a7e4c8a
Patrick Rodrigues <148400897+Patrickdevbjj@users.noreply.github.com>
2023-12-12 21:22:55 -0300

    Update cardsPokemon.js

--- assets/js/cardsPokemon.js
+++ assets/js/cardsPokemon.js
@@ -3,7 +3,8 @@ import pokemon from "./fetchPokemon.js";
 // Obtém o elemento onde os nomes dos Pokémon serão inseridos
 const pokemonList = document.getElementById('pokemonList');
 
-const criarCard = (pokemon) => {
+
+    const criarCard = (pokemon) => {
     
     const id = pokemon.id
 
@@ -11,12 +12,11 @@ const criarCard = (pokemon) => {
     card.style="width: 18rem;"
     card.classList.add('card');
 
-    const img = document.createElement('img')
-    img.src = `https://github.com/wellrccity/pokedex-html-js/blob/master/assets/img/pokemons/poke_${id}.gif?raw=true`
-
-
     const conteudo = document.createElement('div')
     conteudo.classList.add('card-body')
+
+    const img = document.createElement('img')
+    img.src = `https://github.com/wellrccity/pokedex-html-js/blob/master/assets/img/pokemons/poke_${id}.gif?raw=true`
 
     // // Cria um novo elemento de parágrafo
     const titulo = document.createElement('h5');
@@ -29,10 +29,10 @@ const criarCard = (pokemon) => {
     titulo.textContent = pokemon.name;
     texto.textContent = id
 
-    card.appendChild(img);
     card.appendChild(conteudo);
     conteudo.appendChild(titulo);
     conteudo.appendChild(texto);
+    conteudo.appendChild(img);
     pokemonList.appendChild(card);
 }
 
