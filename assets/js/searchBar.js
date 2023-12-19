aef987f085f3a4cde8bde126d6885722b59d70db
Lucas Balbueno Vicencio <182300633@senacinformatica.br>
2023-12-19 19:32:44 -0300

    apagando cards antigos no search

--- assets/js/searchBar.js
+++ assets/js/searchBar.js
@@ -2,10 +2,11 @@ import { fetchAndCreatePokemon } from "./fetchPokemon.js";
 
 const input = document.querySelector('#search-input');
 const form = document.querySelector('#searchForm');
+const pokemonList = document.getElementById('pokemonList');
 
 
 form.addEventListener('submit', async(event) => {
     event.preventDefault();
+    pokemonList.textContent = ''
     await fetchAndCreatePokemon(input.value.toLowerCase());
-    
   });
